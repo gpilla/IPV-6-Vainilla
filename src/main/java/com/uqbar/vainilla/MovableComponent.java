8bb57e628ccbd498a9de3f80fec595a946b16869
Nicolas Faure <nikolas.faure@gmail.com>
2014-04-28 20:22:17 -0300

    Se agrego MovableComponent

---
+++ src/main/java/com/uqbar/vainilla/MovableComponent.java
@@ -0,0 +1,36 @@
+package com.uqbar.vainilla;
+
+import com.uqbar.vainilla.utils.Vector2D;
+
+public abstract class MovableComponent<SceneType extends GameScene> extends GameComponent<SceneType>{
+
+	private double speed = 0;
+	private double acceleration=0;
+	private Vector2D vector;
+	
+	
+	
+	public double getSpeed() {
+		return speed;
+	}
+	
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+	public double getAcceleration() {
+		return acceleration;
+	}
+	public void setAcceleration(double acceleration) {
+		this.acceleration = acceleration;
+	}
+
+	public Vector2D getVector() {
+		return vector;
+	}
+
+	public void setVector(Vector2D vector) {
+		this.vector = vector;
+	}
+	
+	
+}
