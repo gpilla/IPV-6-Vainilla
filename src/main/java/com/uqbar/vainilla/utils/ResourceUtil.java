baafc6a9682cea2bc8f9bae3f5bfed0f3502f19f
Nicolas Faure <nikolas.faure@gmail.com>
2014-06-29 20:58:28 -0300

    Se modifico ResouceUtil para que levante las configuraciones en es

--- src/main/java/com/uqbar/vainilla/utils/ResourceUtil.java
+++ src/main/java/com/uqbar/vainilla/utils/ResourceUtil.java
@@ -1,10 +1,11 @@
 package com.uqbar.vainilla.utils;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ResourceUtil {
 
-	private static ResourceBundle properties = ResourceBundle.getBundle("config");
+	private static ResourceBundle properties = ResourceBundle.getBundle("config",new Locale("es"));
 
 	public static String getResourceString(String name){
 		return properties.getString(name);
@@ -12,6 +13,10 @@ public class ResourceUtil {
 	
 	public static int getResourceInt(String name){
 		return Integer.parseInt(properties.getString(name));
+	}
+	
+	public static double getResourceDouble(String name){
+		return Double.parseDouble(properties.getString(name));
 	}
 	
 	
